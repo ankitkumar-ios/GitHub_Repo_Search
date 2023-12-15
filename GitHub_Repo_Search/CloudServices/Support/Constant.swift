a83dbf8236c10f6a973a0bede9d57e70059eb73f
Ankit Kumar <>
2023-12-15 17:43:38 +0530

    Added ServiceHelper for parsing the response

--- GitHub_Repo_Search/CloudServices/Support/Constant.swift
+++ GitHub_Repo_Search/CloudServices/Support/Constant.swift
@@ -13,3 +13,12 @@ struct Constant {
     static let language = "language"
     static let colon = ":"
 }
+
+public enum HTTPStatusCode: Int {
+    case ok = 200
+    case badRequest = 400
+    case unauthorized = 401
+    case forbidden = 403
+    case notFound = 404
+    case internalServerError = 500
+}
